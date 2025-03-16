ae6faa1daf019c3e50abaa62cbaae592003c9976
Bryan Veloso <bryan@velo.so>
2025-03-16 10:28:28 -0700

    Add crosspost and legacy disclaimer components; update post layout.

---
+++ apps/avalonstar/src/components/legacy-icon.tsx
@@ -0,0 +1,11 @@
+export const LegacyIcon = ({ className }: { className?: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className={className}>
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M1 2c0 -0.55228 0.44772 -1 1 -1h15c0.2652 0 0.5196 0.10536 0.7071 0.29289l5 5C22.8946 6.48043 23 6.73478 23 7v15c0 0.5523 -0.4477 1 -1 1H2c-0.55228 0 -1 -0.4477 -1 -1V2Zm13 1H7v4h7V3ZM5 3v5c0 0.55228 0.44772 1 1 1h9c0.5523 0 1 -0.44772 1 -1V3h0.5858L21 7.41421V21H3V3h2Zm7 16c1.933 0 3.5 -1.567 3.5 -3.5S13.933 12 12 12s-3.5 1.567 -3.5 3.5S10.067 19 12 19Z"
+      clipRule="evenodd"
+      strokeWidth={1}
+    />
+  </svg>
+)
