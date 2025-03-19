f18f1005ece104bb6be59edc804527377b635f58
Bryan Veloso <bryan@velo.so>
2025-03-19 14:58:03 -0700

    Render Omnyist as a static site.

--- apps/omnyist/astro.config.mjs
+++ apps/omnyist/astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,8 +11,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static'
 })
