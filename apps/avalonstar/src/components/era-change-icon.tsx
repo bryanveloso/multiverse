0bf1b8d62d0337c33e51afc1ca686ebb7d73a55c
Bryan Veloso <bryan@velo.so>
2025-06-09 09:52:56 -0700

    Fix that warning.

--- apps/avalonstar/src/components/era-change-icon.tsx
+++ apps/avalonstar/src/components/era-change-icon.tsx
@@ -1,5 +1,5 @@
 export const EraChangeIcon = ({ className }: { className: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class={className}>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className={className}>
     <path
       fill="currentColor"
       fillRule="evenodd"
