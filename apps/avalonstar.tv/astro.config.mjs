3a7d52f006e22f1fb20bbf2604f5cdfe75084a03
Bryan Veloso <bryan@velo.so>
2025-08-10 11:09:26 -0700

    Set up avalonstar.tv with minimal dependencies.

--- apps/avalonstar.tv/astro.config.mjs
+++ apps/avalonstar.tv/astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 5324
-  }
+  },
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwind()]
+  },
+  output: 'static'
 });
--- apps/avalonstar.tv/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-import sharedConfig from '@multiverse/config/tailwind'
-
-const config: Config = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  presets: [sharedConfig]
-}
-
-export default config
